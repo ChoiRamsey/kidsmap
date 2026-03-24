5e2ef9edd714475d5eb0e4e9c52d6b3519348298
choiramsey <choiramsey8@gmail.com>
2026-03-24 14:36:31 +0900

    initial project setup

---
+++ src/main/java/com/example/kidsmap/KidsmapApplication.java
@@ -0,0 +1,13 @@
+package com.example.kidsmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kidsmap/KidsmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kidsmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
